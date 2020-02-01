eea447724a184c27590af48cfa9da2b5b5d85f9c
Blaise <blaze.kalonda@gmail.com>
2020-02-01 19:14:03 +0100

    build folder

---
+++ client/build/precache-manifest.0ba0c346a15e2c1d2bcbfca73a66cdc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8895b8610ae956b03f61ccfd2ddac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c50a9e38564575630e9e",
+    "url": "/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "c50a9e38564575630e9e",
+    "url": "/static/js/2.14e0e502.chunk.js"
+  },
+  {
+    "revision": "2382503fb06ae02de09f27dbbdc02b9a",
+    "url": "/static/js/2.14e0e502.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2caa7b52aed19cc6449c",
+    "url": "/static/js/main.5bc66a9a.chunk.js"
+  },
+  {
+    "revision": "10a4ae085375539bcdad",
+    "url": "/static/js/runtime-main.3851fe69.js"
+  }
+]);
