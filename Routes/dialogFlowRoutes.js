eb1d79c750d603c42a52e214b0e1b9cf78207371
Blaise <blaze.kalonda@gmail.com>
2020-02-02 22:39:07 +0100

    dialogflow route

--- Routes/dialogFlowRoutes.js
+++ Routes/dialogFlowRoutes.js
@@ -7,15 +7,15 @@ module.exports = app => {
     //     res.send('Good Morning');
     // });
     
-    app.post('/api/df_text_query', async (req,res) => {
-        
-       let responses = await chatbot.textQuery(req.body.text, req.body.userID, req.body.parameters);
-            res.send(responses[0].queryResult);
-    });
-    
-    app.post('/api/df_event_query', async (req,res) => {
+    app.post('/api/df_text_query', async (req, res) => {
 
+        let responses = await chatbot.textQuery(req.body.text, req.body.userID, req.body.parameters);
+        res.send(responses[0].queryResult);
+
+    });
+
+    app.post('/api/df_event_query', async (req, res) => {
         let responses = await chatbot.eventQuery(req.body.event, req.body.userID, req.body.parameters);
-            res.send(responses[0].queryResult);
+        res.send(responses[0].queryResult);
     });
 }
